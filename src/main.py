930247be3fdb4404558681813c6bb538746e131c
zsbcn <inzhang@foxmail.com>
2025-11-23 00:06:47 +0800

    优化鉴权接口的逻辑;增加env模板文件

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import json
+import os
 import re
 from contextlib import asynccontextmanager
 
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
 from loguru import logger
 from starlette_session import SessionMiddleware
 
@@ -11,6 +14,8 @@ from conf import SQLModel, engine, WHITE_LIST, Session
 from core import all_routers
 from core.login import LoginConstants
 from model.User import User
+
+system_routes = []
 
 
 @asynccontextmanager
@@ -20,6 +25,9 @@ async def lifespan(app: FastAPI):
     :param app:
     :return:
     """
+    global system_routes
+    load_dotenv()
+    system_routes = [route for route in app.routes if type(route) == APIRoute]
     SQLModel.metadata.create_all(engine)
     logger.info("setup complete")
     yield
@@ -36,31 +44,28 @@ async def check_user(request: Request, call_next):
     :param call_next:
     :return:
     """
-    system_path = False
-    need_session = True
+    global system_routes
+    need_session = False
     # 判断接口请求是否是系统内部接口
-    for router in app.routes:
-        if request.url.path == router.__getattribute__("path"):
-            system_path = True
+    for router in system_routes:
+        path = request.url.path
+        if path != router.__getattribute__("path"):
+            continue
+        if need_session := any([re.match(r, path) for r in WHITE_LIST]):
             break
-    if system_path:
-        for r in WHITE_LIST:
-            if re.match(r, request.url.path):
-                need_session = False
-                break
     # 如果是系统的接口请求，不在白名单内的检查session
-    if system_path and need_session:
+    if need_session:
         current_session = request.session
         if not current_session:
             code, msg = LoginConstants.USER_NOT_LOGIN
-            return Response(content=json.dumps({"code": code, "msg": msg}), status_code=200,
+            return Response(content=json.dumps({"code": code, "msg": msg}, ensure_ascii=False), status_code=200,
                             media_type="application/json")
         user_id = current_session.get("userId")
         with Session(engine) as session:
             user = session.get(User, user_id)
         if not user:
             code, msg = LoginConstants.USER_NOT_EXIST
-            return Response(content=json.dumps({"code": code, "msg": msg}), status_code=200,
+            return Response(content=json.dumps({"code": code, "msg": msg}, ensure_ascii=False), status_code=200,
                             media_type="application/json")
     response: Response = await call_next(request)
     return response
@@ -82,7 +87,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 # 添加session的中间件
-app.add_middleware(SessionMiddleware, secret_key="xxxxxx", cookie_name="sessionId", max_age=3600)
+app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"), cookie_name="sessionId", max_age=3600)
 
 if __name__ == '__main__':
     from uvicorn import run
